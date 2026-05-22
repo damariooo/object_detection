88ca832e6c52e264fb1cdd29aea8e671e110c148
hyodouuuuu <lordharem90@gmail.com>
2026-05-22 21:42:39 +0700

    coba tanpa ai

--- main.py
+++ main.py
@@ -1,42 +1,7 @@
-from fastapi import FastAPI, UploadFile, File
-from ultralytics import YOLO
-import numpy as np
-import cv2
+from fastapi import FastAPI
 
 app = FastAPI()
 
-model = YOLO("best.pt")
-
 @app.get("/")
 def home():
-    return {"status": "AI RUNNING"}
-
-@app.post("/detect")
-async def detect(file: UploadFile = File(...)):
-
-    contents = await file.read()
-
-    nparr = np.frombuffer(contents, np.uint8)
-
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
-    results = model(img)
-
-    detections = []
-
-    for r in results:
-
-        if r.boxes is None:
-            continue
-
-        for box in r.boxes:
-
-            detections.append({
-                "class": int(box.cls[0]),
-                "confidence": float(box.conf[0])
-            })
-
-    return {
-        "status": "success",
-        "detections": detections
-    }
+    return {"status": "RUNNING"}
