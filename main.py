45b04ca4bb18462c1f1fdeffb3f965ce7fbd4d9f
hyodouuuuu <lordharem90@gmail.com>
2026-05-22 21:52:49 +0700

    coba ai

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from ultralytics import YOLO
 
 app = FastAPI()
 
+model = YOLO("best.pt")
+
 @app.get("/")
 def home():
-    return {"status": "RUNNING"}
+    return {"status": "MODEL LOADED"}
